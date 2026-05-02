60948e17631638fd4ccd96e316b0c1dc3036cd3d
CohWerNo <buzdalovdima78@gmail.com>
2026-05-02 22:27:48 +0300

    Description added

--- source/tools/incompleteWindow.js
+++ source/tools/incompleteWindow.js
@@ -1,21 +1,34 @@
+/**
+ * Создает неполное окно, которая перекрывает весь контент что находится за этим окном
+ */
 class incompleteWindow{
+    /** Основное окно */
     #mainWindow = document.createElement("window")
+    /** Основное окно */
     get mainWindow() {
         return this.#mainWindow;
     }
+    /** Тень под окном, которая перекрывает весь контент за ним */
     #shadowBG = document.createElement("div");
+    /** Тень под окном, которая перекрывает весь контент за ним */
     get shadowBG() {
         return this.#shadowBG;
     }
+    /** Верхняя панель с инструментами */
     #topTools_window = document.createElement("topTools");
+    /** Верхняя панель с инструментами */
     get topTools_window() {
         return this.#topTools_window;
     }
+    /** Элементы находящюеся в верхней панели */
     #topTools_elements = {}
+    /** Элементы находящюеся в верхней панели */
     get topTools_elements() {
         return this.#topTools_elements;
     }
+    /** Дополнительное окно, в котором должны находится все элементы, для их отображения у пользователя */
     #windowContent = document.createElement("windowContent");
+    /** Дополнительное окно, в котором должны находится все элементы, для их отображения у пользователя */
     get windowContent() {
         return this.#windowContent;
     }
@@ -38,10 +51,19 @@ class incompleteWindow{
         this.topTools_addElement("exitButton", exitButton);
     }
     
+    /**
+     * Отображает окно у пользователя
+     */
     openWindow() {
         this.#shadowBG.append(this.#mainWindow);
         document.body.append(this.#shadowBG);
     }
+    /**
+     * Добавляет элемент в верхнюю панель
+     * 
+     * `<string> propertyKey = "property" -- устанавливает ключь для значения, по которому этот элемент можно найти в "topTools_elements"`\
+     * `<HTMLElement> value = HTMLElement -- элемент будет установлен в "topTools_window"`
+     */
     topTools_addElement(propertyKey="property", value=HTMLElement) {
         Object.defineProperty(this.#topTools_elements, propertyKey, {value:value, writable:false});
         this.#topTools_window.append(value);
